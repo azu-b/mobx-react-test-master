02938905e2e74795e7fd94db8e5f0bb8ffb4faad
Azu Barraza <azu.barraza.jim@gmail.com>
2021-05-30 14:18:18 -0500

    refactor: Move parseArrayBinTree to TreeInput

--- src/TreeInput.tsx
+++ src/TreeInput.tsx
@@ -8,6 +8,12 @@ interface TreeInputState {
     treeText: string
 }
 
+type BinTreeArray = [
+    root: string | number,
+    leftChild?: BinTreeArray | null,
+    rightChild?: BinTreeArray | null
+];
+
 export class TreeInput extends React.Component<TreeInputProps, TreeInputState>{
     constructor(props: TreeInputProps) {
         super(props);
@@ -21,21 +27,52 @@ export class TreeInput extends React.Component<TreeInputProps, TreeInputState>{
      * @param arrayFormat [id, leftChild, rightChild] for example [1, [2], [3, null, [5]]]
      * @returns TreeNode format
      * */
-    parseArrayToTree(arrayFormat: any[]): BinTreeNode {
-        return new BinTreeNode("toBeImplemeted", null, null);
+    parseArrayToTree = (arrayFormat: BinTreeArray) : BinTreeNode => {
+        // Save arrayFormat array length
+        const arrayLength = arrayFormat.length;
+    
+        if (arrayLength === 2) { // If the tree node has 2 elements
+            const [root, leftChild] = arrayFormat;
+    
+            // If left element is null, then the node has no children
+            if (!leftChild) {
+                return new BinTreeNode(root, null, null);
+            } else { // If not, then we call parseArrayBinTree to create the left node
+                return new BinTreeNode(root, this.parseArrayToTree(leftChild), null);
+            }
+        } else if (arrayLength === 3) { // If the tree node has 3 elements
+            const [root, leftChild, rightChild] = arrayFormat;
+    
+            // If the left and right elements are null
+            if (!leftChild && !rightChild) {
+                return new BinTreeNode(root, null, null);
+            } else if (!leftChild && rightChild) { // If left is null, but right is an array
+                return new BinTreeNode(root, null, this.parseArrayToTree(rightChild));
+            } else if (leftChild && !rightChild) { // If left is an array, but right is null
+                return new BinTreeNode(root, this.parseArrayToTree(leftChild), null);
+            } else if (leftChild && rightChild){ // If both left and right are an array
+                return new BinTreeNode(root, this.parseArrayToTree(leftChild), this.parseArrayToTree(rightChild));
+            }
+        }
+        
+        // If the tree node has no children, array has only one element
+        const [root] = arrayFormat;
+        return new BinTreeNode(root, null, null);
     }
 
     convert = () => {
         // After you implement parseArrayToTree above, uncomment the below code
-        // let treeArrayFormat: any[] = JSON.parse(this.state.treeText);
-        // this.props.onChange(this.parseArrayToTree(treeArrayFormat));
+        let treeArrayFormat: BinTreeArray = JSON.parse(this.state.treeText);
+        this.props.onChange(this.parseArrayToTree(treeArrayFormat));
 
         // After you implement parseArrayToTree above, comment the below code
-        let treeNodeFormat: BinTreeNode = JSON.parse(this.state.treeText);
-        this.props.onChange(treeNodeFormat);
+        // let treeNodeFormat: BinTreeNode = JSON.parse(this.state.treeText);
+        // this.props.onChange(treeNodeFormat);
     }
 
     render() {
+        console.log(this.parseArrayToTree(["a", ["b", ["b1"], ["b2", ["b21"], ["b22"]]], ["c", ["c1", ["c11", ["c111"]], ["c12", null, ["c121"]]], ["c2", null, ["c21"]]]]));
+
         return (
             <div>
                 <button onClick={this.convert}>Process</button><br />
